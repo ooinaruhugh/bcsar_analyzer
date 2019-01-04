ab267253776e90804f3efc6b8369e17c9d375599
Kamillo Ferry <git.hugh@gmail.com>
2019-01-04 03:30:08 +0100

    Figuring out the filename table

--- bcsar_info.c
+++ bcsar_info.c
@@ -3,6 +3,10 @@
 #include <inttypes.h>
 #include "bcsar.h"
 
+void print_strg(STRG_header *strg) {
+  
+}
+
 int main(int argc, char const **argv)
 {
   if (argc <= 1){
@@ -22,15 +26,27 @@ int main(int argc, char const **argv)
     fread(header, sizeof(*header), 1, bcsar_file);
 
     // Preparing the 4-byte file magic as a zero-terminated string
-    char *magic = calloc(1,5);
-    *((uint32_t *)magic) = header->magic;
+    char *magic_buffer = calloc(1,5);
+    *((uint32_t *)magic_buffer) = header->magic;
 
-    printf("MAGIC: %s\n", magic);
+    printf("MAGIC: %s\n", magic_buffer);
+    printf("%s endian\n", header->bom == 0xFEFF ? "little" : "big");
     printf("size (as reported in header): 0x%"PRIx32"\n", header->size);
     printf("Location of STRG: 0x%"PRIx32"\n", header->strg_loc);
     printf("Location of INFO: 0x%"PRIx32"\n", header->info_loc);
     printf("Location of FILE: 0x%"PRIx32"\n", header->file_loc);
 
+    puts("\n== STRG partition info ==");
+    STRG_header *strg = malloc(header->strg_length);
+    fseek(bcsar_file, header->strg_loc, SEEK_SET);
+    fread(strg, header->strg_length, 1, bcsar_file);
+    printf("Filename count: %"PRIu32"\n", strg->filename_count);
+
+
+    free(magic_buffer);
+    free(strg);
+    free(header);
+    fclose(bcsar_file);
   }
   return 0;
 }
