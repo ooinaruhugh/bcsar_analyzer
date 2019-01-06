35615167f50847a67bd9b0de32c2e649b64a0dc6
Kamillo Ferry <git.hugh@gmail.com>
2019-01-07 00:19:43 +0100

    Cleaned this unspectacular code up a little bit. There are parts that scream "reusable code" though.

--- bcsar_info.c
+++ bcsar_info.c
@@ -6,23 +6,17 @@
 
 STRG_header *read_strg(BCSAR_header *header, FILE *bcsar)
 {
-  /* STRG_header *strg = calloc(1, sizeof(*strg));
-  fseek(bcsar, header->strg_loc, SEEK_SET);
-  fread(strg, 0x1C, 1, bcsar);
-  strg->offset_table = malloc(strg->length - 0x1C);
-  fread(strg->offset_table, strg->length - 0x1C, 1, bcsar);
-  return strg; */
   STRG_header *strg = calloc(1, header->strg_length);
   fseek(bcsar, header->strg_loc, SEEK_SET);
   fread(strg, header->strg_length, 1, bcsar);
   return strg;
 }
 
-void *read_partition(FILE *bcsar, uint64_t offset, uint64_t size)
+void *read_partition(FILE *file, uint64_t offset, uint64_t size)
 {
   void *partition = calloc(1, size);
-  fseek(bcsar, offset, SEEK_SET);
-  fread(partition, size, 1, bcsar);
+  fseek(file, offset, SEEK_SET);
+  fread(partition, size, 1, file);
   return partition;
 }
 
@@ -30,23 +24,69 @@ void print_strg(STRG_header *strg)
 {
 }
 
-void print_string_table(STRG_header *strg, FILE *output, char delimiter)
+void print_string_table(STRG_header *strg, char *file, char delimiter)
 {
+  FILE *output;
+  char *outfile;
+  if (file)
+  {
+    outfile = calloc(strlen(file) + 10, 1);
+    sprintf(outfile, "%s.list.csv", file);
+
+    output = fopen(outfile, "a");
+
+    fprintf(output, "sep=,\nIndex,Name\n");
+  }
+  else
+  {
+    output = stdout;
+    fprintf(output, "Filename string table\n");
+  }
+
   for (int i = 0; i < strg->filename_count; i++)
   {
     char *filename = (uintptr_t)strg + STRG_HEADER_END + strg->offset_table[i].data_offset;
     fprintf(output, "%8i%c%s\n", i, delimiter, filename);
     fflush(output);
   }
+
+  if (file)
+  {
+    fclose(output);
+    free(outfile);
+  }
 }
 
-void print_lookup_table(STRG_Lookup_table *strg_lookup, FILE *output, char delimiter)
+void print_lookup_table(STRG_Lookup_table *strg_lookup, char *file, char delimiter)
 {
+  char *lookup;
+  FILE *output;
+  if (file)
+  {
+    lookup = calloc(strlen(file) + 12, 1);
+    sprintf(lookup, "%s.lookup.csv", file);
+
+    output = fopen(lookup, "a");
+
+    fprintf(output, "sep=,\nhas_data,bittest_condition,fail_condition_leaf,success_condition_leaf,index,resource_id,resource_type\n");
+  }
+  else
+  {
+    output = stdout;
+    fprintf(output, "String lookup table\n");
+  }
+
   for (int i = 0; i < strg_lookup->count; i++)
   {
     Lookup_entry *entry = strg_lookup->entries + i;
-    fprintf(output, "%8i%c%x%c%x%c%x%c%x%c%x%c%x%c%x\n", i, delimiter, entry->has_data, delimiter, entry->bit_test, delimiter, entry->fail_leaf_index, delimiter, entry->success_leaf_index, delimiter, entry->lookup_index, delimiter, entry->res_id.id, delimiter, entry->res_type);
+    fprintf(output, "%8i%c%08x%c%08x%c%08x%c%08x%c%08x%c%08x%c%08x\n", i, delimiter, entry->has_data, delimiter, entry->bit_test, delimiter, entry->fail_leaf_index, delimiter, entry->success_leaf_index, delimiter, entry->lookup_index, delimiter, entry->res_id.id, delimiter, entry->res_type);
     fflush(output);
+  }
+
+  if (file)
+  {
+    fclose(output);
+    free(lookup);
   }
 }
 
@@ -97,24 +137,16 @@ int main(int argc, char const **argv)
     puts("\n== INFO partition info ==");
     Info_table *info = read_partition(bcsar_file, header->info_loc, header->info_length);
     printf("Length of partition: %x\n", info->length);
-    // puts("# File list");
 
-    /* char *filelist_name = calloc(strlen(argv[1])+10, 1);
-    sprintf(filelist_name, "%s.list.csv", argv[1]);
-    FILE *filelist = fopen(filelist_name, "a"); 
-    fprintf(filelist, "sep=,\nIndex,Name\n");
-    print_string_table(strg, filelist, ',');
-    fclose(filelist);
-    free(filelist_name); */
-    // print_string_table(strg, stdout, ' ');
+    // Prints file name table
 
-    char *lookup = calloc(strlen(argv[1])+12, 1);
-    sprintf(lookup, "%s.lookup.csv", argv[1]);
-    FILE *lookup_csv = fopen(lookup, "a"); 
-    fprintf(lookup_csv, "sep=,\nhas_data,bittest_condition,fail_condition_leaf,success_condition_leaf,index,resource_id,resource_type\n");
-    print_lookup_table(lookup_table, lookup_csv, ',');
-    fclose(lookup_csv);
-    free(lookup);
+    // print_string_table(strg, argv[1], ','); // To CSV
+    // print_string_table(strg, NULL, ' '); // To stdout
+
+    // Prints the raw string lookup table
+    
+    // print_lookup_table(lookup_table, argv[1], ','); // To CSV
+    // print_lookup_table(lookup_table, NULL, ' '); // To stdout
 
     free(magic_buffer);
     free(strg);
